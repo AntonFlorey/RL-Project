more ppo bug fixes (gae calculation)

part2/ppo.py
@@ -30,8 +30,8 @@ class Policy(nn.Module):
         )
         # Task 1: Implement actor_logstd as a learnable parameter
         # Use log of std to make sure std doesn't become negative during training
-        #self.actor_logstd  = torch.ones(action_dim, device=device) * np.log(0.5)
-        self.actor_logstd = torch.zeros(action_dim, device=device)
+        self.actor_logstd  = torch.ones(action_dim, device=device) * np.log(0.35)
+        #self.actor_logstd = torch.zeros(action_dim, device=device)
 
     def forward(self, state):
         # Get mean of a Normal distribution (the output of the neural network)
@@ -98,11 +98,9 @@ class PPO(object):
         
         deltas = [(r + (1.0 - d) * self.gamma * nv - v) for r, nv, v, d in zip(rewards, values, next_values, dones)]
         deltas = torch.stack(deltas).to(device).squeeze(-1)
-        gaes = deltas.clone()
-
-        for t in reversed(range(len(deltas - 1))):
-            gaes[t] = gaes[t] + (1 - dones[t]) * (self.gae_lambda * self.gamma * gaes[t])
-
+        gaes = deltas.clone().detach()
+        for t in reversed(range(len(deltas) - 1)):
+            gaes[t] = gaes[t] + (1 - dones[t]) * (self.gae_lambda * self.gamma * gaes[t+1])
         return gaes
 
     def _update(self, batch: PPOBatch):
@@ -114,41 +112,24 @@ class PPO(object):
         clipped_ratio = torch.clamp(prob_ratio, 1.0 - self.clip_eps, 1.0 + self.clip_eps)
 
         # scale gae and get value target
-        value_target = batch.dis_rew # batch.gae + new_value
+        value_target = batch.gae + new_value
+        # value_target = batch.dis_rew
         
-        # compute the advantage function TODO: test GAE 
-        adv = batch.dis_rew - new_value.detach()
+        # compute the advantage function
+        adv = batch.gae.clone()
+        #adv = batch.dis_rew - new_value.detach()
+
         # scale if batch is larger than 1 (to prevent nan gradients)
         if adv.shape[0] > 1:
             adv_scaled = (adv - torch.mean(adv)) / (torch.std(adv) + 1e-10)
         else:
+            print("this should not happen anymore!")
             adv_scaled = adv
 
         # compute actor loss 
         l_clip = -torch.mean(torch.min(prob_ratio * adv_scaled.detach(), clipped_ratio * adv_scaled.detach()))
         l_entropy = -torch.mean(new_dist.entropy())
-        actor_loss = l_clip #+ self.entropy_weight * l_entropy 
-
-        if new_value.isnan().any():
-            print("BROKEN VALUE FUNCTION!")
-
-        if adv.isnan().any():
-            print("BROKEN ADVANTAGE!")
-        
-        if adv_scaled.isnan().any():
-            print("BROKEN SCALED ADVANTAGE!")
-            print(adv.shape)
-            print("centered: ", (adv - torch.mean(adv)))
-            print("stddev:", (torch.std(adv) + 1e-10))
-
-        if prob_ratio.isnan().any():
-            print("BROKEN RATIO!")
-
-        if clipped_ratio.isnan().any():
-            print("BROKEN CLIPPED RATIO!")
-
-        if l_clip.isnan().any():
-            print("BROKEN CLIP LOSS!")
+        actor_loss = l_clip + self.entropy_weight * l_entropy 
 
         # compute value loss
         value_loss = F.mse_loss(new_value, value_target.detach())
@@ -171,18 +152,13 @@ class PPO(object):
 
     def update(self,):
         # go over all collected runs 
-
-        # print("collected rewards:", self.rewards)
-
         actions = reduce(lambda a, b: a + b, self.actions.values(), [])
         action_probs = reduce(lambda a, b: a + b, self.action_probs.values(), [])
         rewards = reduce(lambda a, b: a + b, self.rewards.values(), [])
-        # print("rewards listed;", rewards)
         states = reduce(lambda a, b: a + b, self.states.values(), [])
         next_states = reduce(lambda a, b: a + b, self.next_states.values(), [])
         dones = reduce(lambda a, b: a + b, self.dones.values(), [])
         disc = reduce(lambda a, b: a + list(discount_rewards(torch.stack(b, dim=0).to(device).squeeze(-1), self.gamma)), self.rewards.values(), [])
-        # print("discounted rewards:", disc)
         gaes = []
         for run_id in self.actions.keys():
             gae = self._gae(torch.stack(self.rewards[run_id], dim=0).to(device).squeeze(-1), 
@@ -195,7 +171,6 @@ class PPO(object):
         action_probs = torch.stack(action_probs, dim=0) \
                 .to(device).squeeze(-1).squeeze(-1)
         rewards = torch.stack(rewards, dim=0).to(device).squeeze(-1)
-        # print("torch rewards:", rewards)
         states = torch.stack(states, dim=0).to(device).squeeze(-1)
         next_states = torch.stack(next_states, dim=0).to(device).squeeze(-1)
         dones = torch.stack(dones, dim=0).to(device).squeeze(-1)
@@ -221,15 +196,18 @@ class PPO(object):
 
         indices = np.arange(actions.shape[0])
         for ep in range(self.batches_per_episode):
-            # print("sub episode:", ep)
             #randomly shuffle indices 
             np.random.shuffle(indices)
             #create minibatches
             i = 0
             while i < len(indices):
-                ind = indices[i:min(i+self.minibatch_size, len(indices))]
-                # print("curr batch indices:", ind)
-                # print("current batch reward:", rewards[ind])
+                if i + 2 * self.minibatch_size > len(indices):
+                    ind = indices[i:len(indices)]
+                    i = len(indices)
+                else:
+                    ind = indices[i:min(i+self.minibatch_size, len(indices))]
+                    i += self.minibatch_size
+
                 batch = PPOBatch(
                 state=states[ind],
                 action=actions[ind],
@@ -241,7 +219,6 @@ class PPO(object):
                 done=dones[ind]
                 )
                 self._update(batch)
-                i += self.minibatch_size
                 
         return {}
 
@@ -250,14 +227,6 @@ class PPO(object):
         if observation.ndim == 1: observation = observation[None] # add the batch dimension
         x = torch.from_numpy(observation).float().to(device)
 
-        # Task 1
-        ########## Your code starts here. ##########
-        # Hints: 1. the self.policy returns a normal distribution, check the PyTorch document to see 
-        #           how to calculate the log_prob of an action and how to sample.
-        #        2. if evaluation, return mean, otherwise, return a sample
-        #        3. the returned action and the act_logprob should be the torch.Tensors.
-        #            Please always make sure the shape of variables is as you expected.
-        
         with torch.no_grad():
             probs = self.policy.forward(x)
             action = 0
@@ -268,10 +237,8 @@ class PPO(object):
                 action =  probs.sample()
 
             # clamp action (bipedalwalker specific)
-            # action = torch.clamp(action, -1.0, 1.0)
+            action = torch.clamp(action, -1.0, 1.0)
             act_logprob = probs.log_prob(action)
-
-            ########## Your code ends here. ###########
 
         return action, act_logprob.sum(dim=-1, keepdim=True)
 

part2/train_ppo.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(".."))
-os.environ["MUJOCO_GL"] = "glfw" # for mujoco rendering
+os.environ["MUJOCO_GL"] = "egl" #"glfw" # for mujoco rendering
 import time
 from pathlib import Path
 import json
@@ -23,16 +23,19 @@ def to_numpy(tensor):
     return tensor.cpu().numpy().flatten()
 
 # Policy training function
-def train(agent: PPO, env, max_episode_steps=512, runs_per_episode=32):
+def train(agent: PPO, env, max_episode_steps=512, runs_per_episode=32, min_steps=1000):
     collected_reward_sums = []
     collected_timesteps = []
-    for run_id in range(runs_per_episode):
+    run = 0
+    steps = 0
+    while (run < runs_per_episode or steps < min_steps):
         # Run actual training        
         reward_sum, timesteps, done, episode_timesteps = 0, 0, False, 0
         # Reset the environment and observe the initial state
         obs = env.reset()
         while not done:
             episode_timesteps += 1
+            steps += 1
             
             # Sample action from policy
             action, act_logprob = agent.get_action(obs)
@@ -44,7 +47,7 @@ def train(agent: PPO, env, max_episode_steps=512, runs_per_episode=32):
                 done = True
 
             # Store action's outcome (so that the agent can improve its policy)
-            agent.record(obs, action, act_logprob, reward, done, next_obs, run_id)
+            agent.record(obs, action, act_logprob, reward, done, next_obs, run)
 
             # Store total episode reward
             reward_sum += reward
@@ -52,7 +55,7 @@ def train(agent: PPO, env, max_episode_steps=512, runs_per_episode=32):
 
             # update observation
             obs = next_obs.copy()
-        
+        run += 1
         collected_reward_sums.append(reward_sum)
         collected_timesteps.append(timesteps)
 
@@ -60,7 +63,7 @@ def train(agent: PPO, env, max_episode_steps=512, runs_per_episode=32):
     info = agent.update()
 
     # Return stats of training
-    info.update({'timesteps': np.mean(collected_timesteps),
+    info.update({'timesteps': np.sum(collected_timesteps),
                 'ep_reward': np.mean(collected_reward_sums),})
     return info
 
@@ -122,6 +125,12 @@ def main(cfg):
 
    # create a env
     env = make_env.create_env(cfg.env_name, seed=cfg.seed)
+    if cfg.save_video and not cfg.testing:
+        ep_trigger = 100
+        video_path = work_dir/'video'/'train'
+        env = gym.wrappers.RecordVideo(env, video_path,
+                                        episode_trigger=lambda x: x % ep_trigger == 0,
+                                        name_prefix=f'{cfg.agent}')
 
     state_shape = env.observation_space.shape
     action_dim = env.action_space.shape[0]
@@ -163,7 +172,7 @@ def main(cfg):
             for ep in range(cfg.train_episodes + 1):
                 # collect data and update the policy
                 start = time.thread_time()
-                train_info = train(agent, env, cfg.horizon, cfg.num_agents)
+                train_info = train(agent, env, cfg.horizon, cfg.num_agents, cfg.min_steps_per_ep)
                 end = time.thread_time()
                 train_time += (end - start)
 
@@ -233,9 +242,26 @@ def main(cfg):
                 print('Testing (seed='+ str(seed) + ') ...')
                 test(agent, env, num_episode=50)
 
+# gamma = 0.99
+# lam = 0.95
+
+# def gae(rewards, values, next_values, dones):
+#     deltas = [(r + (1.0 - d) * gamma * nv - v) for r, nv, v, d in zip(rewards, values, next_values, dones)]
+#     print("deltas: ", deltas)
+
+#     gaes = deltas.copy()
+#     for t in reversed(range(len(deltas)-1)):
+#         print(t)
+#         gaes[t] = gaes[t] + (1 - dones[t]) * (lam * gamma * gaes[t+1])
+#     print("gaes: ", gaes)
 
 # Entry point of the script
 if __name__ == "__main__":
+    # rew = [1,1,1,1]
+    # values = [0,0,0,0]
+    # next_values = [0,0,0,0]
+    # dones = [0,0,0,1]
+    # gae(rew, values, next_values, dones)
     main()
 
 